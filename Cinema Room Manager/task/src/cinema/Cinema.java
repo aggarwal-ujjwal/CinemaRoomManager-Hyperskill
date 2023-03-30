7267b00cbbfb0c265cdc5994d0ba84f1bc47804f
aggarwal-ujjwal <aggarwal.ujjwal.17@gmail.com>
2023-03-31 02:42:10 +0530

    Stage 1 solution

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -4,5 +4,23 @@ public class Cinema {
 
     public static void main(String[] args) {
         // Write your code here
+        final int row = 7;
+        final int col = 8;
+
+        System.out.println("Cinema:");
+        System.out.printf(" ");
+        for (int i = 0; i < col; i++) {
+            System.out.printf(" " + (i + 1));
+        }
+        System.out.println();
+
+        for (int i = 0; i < row; i++) {
+            System.out.print(i+1);
+            for (int j = 0; j < col; j++) {
+                System.out.print(" S");
+            }
+            System.out.println();
+        }
+
     }
 }
