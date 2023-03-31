739ecb8405058ba4d7fb43408bfe609f083d9bce
aggarwal-ujjwal <aggarwal.ujjwal.17@gmail.com>
2023-03-31 20:50:04 +0530

    Stage 3 solution

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -6,43 +6,63 @@ public class Cinema {
 
     public static void main(String[] args) {
         // Write your code here
-//        final int row = 7;
-//        final int col = 8;
-//
-//        System.out.println("Cinema:");
-//        System.out.printf(" ");
-//        for (int i = 0; i < col; i++) {
-//            System.out.printf(" " + (i + 1));
-//        }
-//        System.out.println();
-//
-//        for (int i = 0; i < row; i++) {
-//            System.out.print(i+1);
-//            for (int j = 0; j < col; j++) {
-//                System.out.print(" S");
-//            }
-//            System.out.println();
-//        }
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Enter the number of rows:");
         int rows = scanner.nextInt();
 
         System.out.println("Enter the number of seats in each row:");
         int noOfSeats = scanner.nextInt();
+        
+        System.out.println("Cinema:");
+        System.out.printf(" ");
+        for (int i = 0; i < noOfSeats; i++) {
+            System.out.printf(" " + (i + 1));
+        }
+        System.out.println();
 
-        System.out.println("Total income:");
+        for (int i = 0; i < rows; i++) {
+            System.out.print(i+1);
+            for (int j = 0; j < noOfSeats; j++) {
+                System.out.print(" S");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("Enter a row number:");
+        int rowNum = scanner.nextInt();
+
+        System.out.println("Enter a seat number in that row:");
+        int seatNum = scanner.nextInt();
+
+
         int totalSeats = rows * noOfSeats;
         if (totalSeats <= 60) {
-            System.out.println("$" + totalSeats * 10);
+            System.out.println("Ticket price: $" + 10);
         } else {
-            int frontRows = rows / 2;
-            int backRows = rows - frontRows;
-
-            System.out.println("$" + (frontRows * noOfSeats * 10 + backRows * noOfSeats * 8))
-            ;
-
+            if (rowNum <= rows / 2) {
+                System.out.println("Ticket price: $" + 10);
+            } else {
+                System.out.println("Ticket price: $" + 8);
+            }
         }
 
+        System.out.println("Cinema:");
+        System.out.printf(" ");
+        for (int i = 0; i < noOfSeats; i++) {
+            System.out.printf(" " + (i + 1));
+        }
+        System.out.println();
+
+        for (int i = 0; i < rows; i++) {
+            System.out.print(i+1);
+            for (int j = 0; j < noOfSeats; j++) {
+                if(rowNum==i+1 && seatNum==j+1){
+                    System.out.print(" B");
+                } else {
+                    System.out.print(" S");
+                }
+            }
+            System.out.println();
+        }
     }
 }
